c1dda9662fde8b06b51f1b10aa6f061ea72eece5
p-t-a-p-1 <kb240225+1@gmail.com>
2020-12-14 23:28:37 +0900

    [update] sequelizeでphotoテーブルのマイグレーションファイル作成

---
+++ migrations/20201214142330-create-photo.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Photos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      label: {
+        type: Sequelize.STRING
+      },
+      photo_url: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Photos');
+  }
+};
