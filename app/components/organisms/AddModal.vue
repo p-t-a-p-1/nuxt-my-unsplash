002b332767e5c81dab66ef1ce95c6876f46f9b35
p-t-a-p-1 <kb240225+1@gmail.com>
2021-02-09 23:27:44 +0900

    [update] 追加モーダルをコンポーネント化

--- app/components/organisms/AddModal.vue
+++ app/components/organisms/AddModal.vue
@@ -1,29 +1,66 @@
 <template>
-  <div>
-    <header class="header">
-      <div class="header_left">
-        <nuxt-link to="/" class="header_left_logo">
-          <img src="@/assets/img/my_unsplash_logo.svg" />
-        </nuxt-link>
-        <div class="header_left_search">
-          <input type="text" placeholder="Search by name" />
+  <div v-show="isModal" class="addModal">
+    <div class="addModal_wrap">
+      <p class="addModal_wrap_title">Add a new photo</p>
+      <form class="addModal_wrap_form form" @submit.prevent="photoSubmit">
+        <dl class="form_wrap">
+          <div class="form_wrap_content -label">
+            <dt>
+              <label for="label">Label</label>
+            </dt>
+            <dd>
+              <input
+                id="label"
+                v-model="photoForm.label"
+                type="text"
+                name="label"
+                placeholder="Suspendisse elit massa"
+              />
+            </dd>
+          </div>
+          <div class="form_wrap_content -label">
+            <dt>
+              <label for="url">Photo URL</label>
+            </dt>
+            <dd>
+              <input
+                id="url"
+                v-model="photoForm.src"
+                type="text"
+                name="url"
+                placeholder="https://images.unsplash.com/photo-1584395630827-860eee694d7b?ixlib=r..."
+              />
+            </dd>
+          </div>
+        </dl>
+        <div class="addModal_wrap_buttonWrap">
+          <button class="modalButton -cancel" @click="isModal = !isModal">
+            Cancel
+          </button>
+          <button type="submit" class="modalButton -submit">
+            Submit
+          </button>
         </div>
-      </div>
-      <div class="header_right">
-        <button class="header_right_button" @click="isModal = !isModal">
-          Add a photo
-        </button>
-      </div>
-    </header>
-    <add-modal :isModal="isModal"></add-modal>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'AddModal',
+  props: {
+    isModal: {
+      type: Boolean,
+      default: false
+    }
+  },
   data() {
     return {
-      isModal: false
+      photoForm: {
+        label: '',
+        src: ''
+      }
     }
   },
   methods: {
@@ -46,60 +83,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.header {
-  margin: auto;
-  padding-top: 32px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 95%;
-  &_left {
-    display: flex;
-    align-items: center;
-    &_search {
-      margin-left: 32px;
-      input {
-        padding-left: 56px;
-        width: 300px;
-        height: 55px;
-        border: 1px solid #bdbdbd;
-        box-sizing: border-box;
-        filter: drop-shadow(0px 1px 6px rgba(0, 0, 0, 0.1));
-        border-radius: 12px;
-      }
-    }
-  }
-  &_right {
-    &_button {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 137px;
-      height: 55px;
-      cursor: pointer;
-      font: {
-        family: Noto Sans;
-        style: normal;
-        weight: bold;
-        size: 16px;
-      }
-      line-height: 22px;
-      /* identical to box height */
-      color: #fff;
-      border: none;
-      background: #3db46d;
-      box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
-      border-radius: 12px;
-      transition: 0.3s;
-      &:hover {
-        border: 1px solid #3db46d;
-        color: #3db46d;
-        background-color: #fff;
-      }
-    }
-  }
-}
+<style lang="scss" scoped>
 .addModal {
   position: fixed;
   top: 0;
